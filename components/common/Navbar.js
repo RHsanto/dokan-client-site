1a40ecdb38333a0e421ae0b88513f54bdefe0afd
RHsanto <rakibulsanto789@gmail.com>
2023-05-15 00:18:17 +0600

    done nav

--- components/common/Navbar.js
+++ components/common/Navbar.js
@@ -1,47 +1,40 @@
 import Link from "next/link";
 import { RiSearch2Line, RiShoppingCartLine } from "react-icons/ri";
 import { BiUser } from "react-icons/bi";
+
 const Navbar = () => {
+
+
   return (
     <div className="container mx-auto px-32 h-20  flex justify-between items-center">
       <div className="logo">
         <Link href="/">
-          <h4>Dokan</h4>
+          <h4 >Dokan</h4>
         </Link>
       </div>
-      <div className="font-medium">
-        <li className="hover:bg-offWhite py-2 rounded-full">
-          <a>
-            <Link href="/men">Men</Link>
-          </a>
+
+      <div> <div className="font-medium">
+        <li className="hover:bg-offWhite py-2 rounded-full">         
+            <Link href="/men">Men</Link>       
+        </li>
+        <li className="hover:bg-offWhite mx-5 py-2 rounded-full">        
+            <Link href="/women">Women</Link>          
         </li>
         <li className="hover:bg-offWhite py-2 rounded-full">
-          <a>
-            <Link href="/women">Women</Link>
-          </a>
-        </li>
-        <li className="hover:bg-offWhite py-2 rounded-full">
-          <a>
-            {" "}
             <Link href="/beauty">Beauty</Link>
-          </a>
         </li>
-        <li className="hover:bg-offWhite py-2 rounded-full">
-          <a>
+        <li className="hover:bg-offWhite mx-5 py-2 rounded-full">
             <Link href="/sport">Sport</Link>
-          </a>
         </li>
-        <li className="hover:bg-offWhite py-2 rounded-full">
-          <a>
+        <li className="hover:bg-offWhite  py-2 rounded-full">
             <Link href="/contact">Contact</Link>{" "}
-          </a>
         </li>
-      </div>
+       </div></div>
       <div>
-        <li className="text-2xl  border-2 border-black rounded-full p-2">
+        {/* <li  className="text-2xl  border-2 border-black rounded-full p-2">
           <RiSearch2Line />
-        </li>
-        <li className="mx-2 text-2xl  border-2 border-black rounded-full p-2">
+        </li> */}
+        <li className="mr-3 text-2xl  border-2 border-black rounded-full p-2">
           <BiUser />
         </li>
         <li className="text-2xl  border-2 border-black rounded-full p-2">
@@ -50,6 +43,7 @@ const Navbar = () => {
       </div>
     </div>
   );
+  
 };
 
 export default Navbar;
