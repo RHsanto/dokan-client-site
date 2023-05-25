b31fb3fb00e2b7f05b9a7aaa09e029a777f1bbec
RHsanto <rakibulsanto789@gmail.com>
2023-05-25 11:46:07 +0600

    done discover

--- components/Home/Discover.js
+++ components/Home/Discover.js
@@ -1,6 +1,6 @@
-import Image from "next/image";
-import img1 from "../../images/4.png";
+/* eslint-disable @next/next/no-img-element */
 import Slider from "react-slick";
+
 const Discover = () => {
   var settings = {
     dots: true,
@@ -8,29 +8,39 @@ const Discover = () => {
     speed: 500,
     slidesToScroll: 1,
     slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
-const discoData = [
-  {id:"01",
-  title: "",
-  image:{img1},
-  bg:"#E3FFE6"
-  },
-  {id:"02",
-  title: "",
-  image:{img1},
-  bg:"#E79F01"
-  },
-  {id:"03",
-  title: "",
-  image:{img1},
-  bg:"#E79F01"
-  },
-  {id:"04",
-  title: "",
-  image:{img1},
-  bg:"#E79F01"
-  }
-]
+  const discoData = [
+    { id: "03", image:'https://i.ibb.co/mbrRGry/4.png', bg: "#FEE2E2" },
+    { id: "01", image:'https://i.ibb.co/thz2GFv/3.png', bg: " #EFF6FF" },
+    { id: "02", image:'https://i.ibb.co/s5jN4bd/1.png', bg: "#FEFCE8" },
+    { id: "04", image:'https://i.ibb.co/6g0sSTj/2.png', bg: "#E3FFE6" },
+  ];
   return (
     // <div className=" mx-auto py-32">
     //   <h3 className="pb-10">
@@ -132,63 +142,25 @@ const discoData = [
     //     </div> */}
 
     // </div>
-    <div className="pl-52 -mr-[200px] py-20 w-full overflow-hidden">
-      <h3 className="pb-10">
+    <div className="p-5 lg:pl-52 -mr-[200px] py-20 w-full overflow-hidden">
+      <h3  className="lg:pb-10 pb-20 lg:pt-14 ">
         Discover more. <span className="opacity-50">Good things are waiting for you</span>{" "}
       </h3>
       <Slider {...settings}>
-        {discoData?.map((data)=>
-        <div key={data.id} className="">
-        <div style={{background:data.bg}} className= { ` mx-3 flex p-10 rounded-lg  `} >
-          <div className="relative"> 
-            <p>Explore new arrivals</p>
-            <h4 className="pt-2">Shop the latest from top brands</h4>
-            <button className="common-btn absolute bottom-0">Show me all</button>
-          </div>
-          <div>
-            <Image src={img1} alt="img" />
-          </div>
-        </div>
-      </div>
-        )}
-        
-        {/* <div>
-          <div className="lg:w-[544px] lg:h-[306px] w-full flex p-10 rounded-lg bg-blue">
-            <div className="relative">
-              <p>Explore new arrivals</p>
-              <h4 className="pt-2">Shop the latest from top brands</h4>
-              <button className="common-btn absolute bottom-0">Show me all</button>
-            </div>
-            <div>
-              <Image src={img1} alt="img" />
+        {discoData?.map(data => (
+          <div key={data.id} className="">
+            <div style={{ background: data.bg }} className={` mx-3 flex p-10  rounded-2xl  `}>
+              <div className="relative">
+                <p>Explore new arrivals</p>
+                <h4 className="pt-2">Shop the latest from top brands</h4>
+                <button className="common-btn absolute bottom-0">Show me all</button>
+              </div>
+              <div>
+                <img src={data?.image} alt="img" />
+              </div>
             </div>
           </div>
-        </div>
-        <div>
-          <div className="lg:w-[544px] lg:h-[306px] w-full flex p-10 rounded-lg bg-blue">
-            <div className="relative">
-              <p>Explore new arrivals</p>
-              <h4 className="pt-2">Shop the latest from top brands</h4>
-              <button className="common-btn absolute bottom-0">Show me all</button>
-            </div>
-            <div>
-              <Image src={img1} alt="img" />
-            </div>
-          </div>
-        </div>
-        <div>
-          <div className="lg:w-[544px] lg:h-[306px] w-full flex p-10 rounded-lg bg-blue">
-            <div className="relative">
-              <p>Explore new arrivals</p>
-              <h4 className="pt-2">Shop the latest from top brands</h4>
-              <button className="common-btn absolute bottom-0">Show me all</button>
-            </div>
-            <div>
-              <Image src={img1} alt="img" />
-            </div>
-          </div>
-        </div> */}
-     
+        ))}
       </Slider>
     </div>
   );
