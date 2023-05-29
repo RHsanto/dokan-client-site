1bec77ab7ca5f24408be1d58995d327eadb2ed2d
RHsanto <rakibulsanto789@gmail.com>
2023-05-29 11:33:06 +0600

    add

--- components/Home/NewArrivals.js
+++ components/Home/NewArrivals.js
@@ -1,29 +1,39 @@
 /* eslint-disable @next/next/no-img-element */
 import React, { useState } from "react";
 import img from "../../images/products/1.png";
+import img2 from "../../images/products/2.png";
+import img3 from "../../images/products/3.png";
+import img4 from "../../images/products/4.png";
 import Image from "next/image";
 import { AiFillHeart, AiOutlineHeart, AiFillStar } from "react-icons/ai";
 
 const NewArrivals = () => {
   const [love, setLove] = useState(1);
-
+  const [catColor, setCatColor] = useState("yellow");
   const handleLove = () => {
     setLove(1);
   };
-
   const removeLove = () => {
     setLove(-1);
   };
 
-  // color
+  // category color selector
+  const yellowBtn = () => {
+    setCatColor("yellow");
+  };
+  const blueBtn = () => {
+    setCatColor("blue");
+  };
+  const redBtn = () => {
+    setCatColor("red");
+  };
+  const greenBtn = () => {
+    setCatColor("green");
+  };
+  const blackBtn = () => {
+    setCatColor("black");
+  };
 
-  const colorCategory = [
-    { id: "01", bg: "#FBBF24", dw: "26px", dh: "26px", bw: "20px", bh: "20px", br: "9999px" },
-    { id: "02", bg: "#0EA5E9", dw: "26px", dh: "26px", bw: "20px", bh: "20px", br: "9999px" },
-    { id: "03", bg: "#EF4444", dw: "26px", dh: "26px", bw: "20px", bh: "20px", br: "9999px" },
-    { id: "04", bg: "#66D78F", dw: "26px", dh: "26px", bw: "20px", bh: "20px", br: "9999px" },
-    { id: "05", bg: "#000", dw: "26px", dh: "26px", bw: "20px", bh: "20px", br: "9999px" },
-  ];
 
   return (
     <div className="container mx-auto lg:px-32">
@@ -34,6 +44,7 @@ const NewArrivals = () => {
         <div>
           <div className="bg-bgWhite rounded-2xl relative">
             <Image src={img} alt="img" />
+
             {love === 1 ? (
               <div
                 onClick={removeLove}
@@ -52,40 +63,55 @@ const NewArrivals = () => {
           </div>
           <div className="product_info  p-3 relative">
             <div className="color_category gap-2 flex py-3">
-              {colorCategory.map(data => (
-                <div
-                  key={data?.id}
-                  style={{
-                    border: `2px solid ${data?.bg}`,
-                    width: data?.dw,
-                    height: data?.dh,
-                    borderRadius: data?.br,
-                  }}
-                  className=" p-[1px] "
-                >
-                  <button
-                    style={{
-                      backgroundColor: data?.bg,
-                      width: data?.bw,
-                      height: data?.bh,
-                      borderRadius: data?.br,
-                    }}
-                  ></button>
-                </div>
-              ))}
+              <div
+                className={`${catColor === "yellow" && "border-2 border-yellow"}
+                 rounded-full  p-[1px] w-[26px] h-[26px]`}
+              >
+                <button
+                  onClick={yellowBtn}
+                  className="w-[20px] h-[20px] bg-yellow rounded-full "
+                ></button>
+              </div>
+              <div
+                className={`
+                ${catColor === "blue" && "border-2 border-blue"}          
+                rounded-full  p-[1px] w-[26px] h-[26px]`}
+              >
+                <button
+                  onClick={blueBtn}
+                  className="w-[20px] h-[20px] bg-blue rounded-full "
+                ></button>
+              </div>
 
-              {/* <div className=" p-[1px] w-[26px] h-[26px] border-2 border-red  rounded-full">
-                <button className="w-[20px] h-[20px] bg-red rounded-full "></button>
+              <div
+                className={`
+                ${catColor === "red" && "border-2 border-red"}            
+                  rounded-full  p-[1px] w-[26px] h-[26px]`}
+              >
+                <button
+                  onClick={redBtn}
+                  className="w-[20px] h-[20px] bg-red rounded-full "
+                ></button>
               </div>
-              <div className=" p-[1px] w-[26px] h-[26px] border-2 border-blue  rounded-full">
-                <button className="w-[20px] h-[20px] bg-blue rounded-full "></button>
+              <div
+                className={`${
+                  catColor === "green" && "border-2 border-green"
+                } rounded-full  p-[1px] w-[26px] h-[26px]`}
+              >
+                <button
+                  onClick={greenBtn}
+                  className="w-[20px] h-[20px] bg-green rounded-full "
+                ></button>
               </div>
-              <div className=" p-[1px] w-[26px] h-[26px] border-2 border-green  rounded-full">
-                <button className="w-[20px] h-[20px] bg-green rounded-full "></button>
+              <div
+                className={` ${catColor === "black" && "border-2 border-black"}
+                  rounded-full  p-[1px] w-[26px] h-[26px]`}
+              >
+                <button
+                  onClick={blackBtn}
+                  className="w-[20px] h-[20px] bg-black rounded-full "
+                ></button>
               </div>
-              <div className=" p-[1px] w-[26px] h-[26px] border-2 border-black  rounded-full">
-                <button className="w-[20px] h-[20px] bg-black rounded-full "></button>
-              </div> */}
             </div>
             <div className="product_details py-3">
               <h5>Leather Gloves</h5>
